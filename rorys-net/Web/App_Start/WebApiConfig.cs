bc82a660a12c3bb454b426a1b9cae377b9734d37
richard.rohac <richard.rohac@elso-systems.com>
2021-08-17 09:39:16 +0200

    JWT authentication through custom attribute

--- rorys-net/Web/App_Start/WebApiConfig.cs
+++ rorys-net/Web/App_Start/WebApiConfig.cs
@@ -10,6 +10,8 @@ namespace Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Filters.Add(new AuthorizeAttribute());
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- rorys-net/Web/Controllers/HomeController.cs
+++ rorys-net/Web/Controllers/HomeController.cs
@@ -6,51 +6,46 @@ using System.Net;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Web;
-using System.Web.Mvc;
-using Web.JWT.Managers;
-using Web.JWT.Models;
+using System.Web.Http;
+using Web.JWT;
 
 namespace Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
-        public HttpResponseMessage Index()
+        [HttpGet]
+        [Route("api/home/token")]
+        [JwtAuthentication]
+        public string Get()
         {
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            return "OK";
         }
 
-        public HttpResponseMessage JWTTest()
+        [HttpGet]
+        [Route("api/home/jwt")]
+        [AllowAnonymous]
+        public string JWTTest()
         {
-            IAuthContainerModel model = GetJWTContainerModel("Richard Rohac", "richardrohac496@gmail.com");
-            IAuthService authService = new JWTService(model.SecretKey);
+            //IAuthContainerModel model = GetJWTContainerModel("Richard Rohac", "richardrohac496@gmail.com");
+            //IAuthService authService = new JWTService(model.SecretKey);
 
-            string token = authService.GenerateToken(model);
+            //string token = authService.GenerateToken(model);
 
-            if (!authService.IsTokenValid(token))
-            {
-                throw new UnauthorizedAccessException();
-            }
-            else
-            {
-                List<Claim> claims = authService.GetTokenClaims(token).ToList();
+            //if (!authService.IsTokenValid(token))
+            //{
+            //    throw new UnauthorizedAccessException();
+            //}
+            //else
+            //{
+            //    List<Claim> claims = authService.GetTokenClaims(token).ToList();
 
-                Debug.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value);
-                Debug.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Email)).Value);
-            }
+            //    Debug.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value);
+            //    Debug.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Email)).Value);
+            //}
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            return JWTManager.GenerateToken("richard.rohac");
         }
 
-        private static JWTContainerModel GetJWTContainerModel(string name, string email)
-        {
-            return new JWTContainerModel()
-            {
-                Claims = new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, name),
-                    new Claim(ClaimTypes.Email, email)                   
-                }
-            };
-        }
+        
     }
 }
